c8b80433942307924e083f35cf33d3f94063e3c3
João VIctor <joawrt8520@gmail.com>
2024-08-04 18:06:04 -0300

    feat: ManyToMany association with JoinTable

--- course/src/main/java/com/educandoweb/course/entities/Category.java
+++ course/src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,7 +20,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     // constructors
